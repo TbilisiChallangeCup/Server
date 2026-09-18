af022e8ea05cfc7edafebacd8cfa0d283d285caf
justzeestuff <justsomezeestuff@gmail.com>
2026-09-18 14:39:13 +0400

    addTeam terminal

--- src/scripts/server/calendar.ts
+++ src/scripts/server/calendar.ts
@@ -3,16 +3,16 @@ import { octokit, repo, owner, Input } from '../utils'
 import sharp from 'sharp'
 
 type Team = {
-    "index": number,
-    "team": string,
-    "played": number,
-    "wins": number,
-    "draws": number,
-    "losses": number,
-    "goalsScored": number,
-    "goalsAgainst": number,
-    "goalDifference": number,
-    "points": number
+    index: number,
+    team: string,
+    played: number,
+    wins: number,
+    draws: number,
+    losses: number,
+    goalsScored: number,
+    goalsAgainst: number,
+    goalDifference: number,
+    points: number
 }
 type structure = {
     [year: string]: [Team]
@@ -36,22 +36,35 @@ export class Calendar {
         })
         const answer = await Input('არჩევანი', Number);
 
+        // Terminal
         switch (answer) {
             case 0:
-
-                break;
+                const teamData = {
+                    index: 0,
+                    team: await this.TeamIcon(await Input('გუნდის ფოტო', String)),
+                    played: await Input('ნათამაშები', Number),
+                    wins: await Input('ნათამაშები', Number),
+                    draws: await Input('ნიჩიები', Number),
+                    losses: await Input('წაგებები', Number),
+                    goalsScored: await Input('გატანილი გოლები', Number),
+                    goalsAgainst: await Input('საწინააღმდეგოდ გატანილი გოლები', Number),
+                    points: 0
+                }
+                teamData.points = teamData.wins * 3 + teamData.draws * 1;
+                
+                console.log(teamData)
+            break;
 
             default:
                 break;
         }
     }
 
-    async TeamIcon(path: string) {
+    async TeamIcon(path: string) : Promise<string> {
         const buffer = await sharp(path).resize(500).toBuffer()
         const base64 = buffer.toString('base64')
 
         const name = `images/calendar-${Date.now()}.jpg`
-        const imageUrl = `https://raw.githubusercontent.com/${owner}/${repo}/main/${name}`
 
         await octokit.rest.repos.createOrUpdateFileContents({
             owner: owner!,
@@ -60,7 +73,7 @@ export class Calendar {
             content: base64,
             message: 'ფოტოს ატვირთვა',
         });
-        return imageUrl 
+        return `https://raw.githubusercontent.com/${owner}/${repo}/main/${name}`
     }
 
     async UpdateFile(json: structure, messege: string, sha: string) {
