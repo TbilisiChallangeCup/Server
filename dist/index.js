047526d95d7b08085fe2fa34314333bb142c45ae
justzeestuff <justsomezeestuff@gmail.com>
2026-09-18 16:09:07 +0400

    index ts terminal

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,40 @@
 "use strict";
-// import dotenv from 'dotenv'\
+// import dotenv from 'dotenv'
 // dotenv.config()
 Object.defineProperty(exports, "__esModule", { value: true });
-// new News().Init()
+const index_1 = require("./scripts/utils/index");
+const charts_1 = require("./scripts/server/charts");
+const calendar_1 = require("./scripts/server/calendar");
+const news_1 = require("./scripts/server/news");
 const contacts_1 = require("./scripts/server/contacts");
-new contacts_1.Contact().Init();
+async function main() {
+    const modules = [
+        'გრაფიკები',
+        'კალენდარი',
+        'სიახლეები',
+        'კონტაქტი',
+    ];
+    modules.map((entry, i) => {
+        console.log(`${[i]}.${entry}`);
+    });
+    const answer = await (0, index_1.Input)('არჩევანი', Number);
+    switch (answer) {
+        case 0:
+            await new charts_1.Charts().Init();
+            break;
+        case 1:
+            await new calendar_1.Calendar().Init();
+            break;
+        case 2:
+            await new news_1.News().Init();
+            break;
+        case 3:
+            await new contacts_1.Contact().Init();
+            break;
+        default:
+            console.log('არასწორი არჩევანი');
+            break;
+    }
+}
+main();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,42 @@
-// import dotenv from 'dotenv'\
+// import dotenv from 'dotenv'
 // dotenv.config()
 
-import { Charts } from "./scripts/server/charts";
-// new Charts().Init();
-import { Calendar } from "./scripts/server/calendar";
-// new Calendar().Init();
-import { News } from "./scripts/server/news";
-// new News().Init()
-import { Contact } from "./scripts/server/contacts";
-new Contact().Init();
+import { Input } from './scripts/utils/index'
+import { Charts } from './scripts/server/charts'
+import { Calendar } from './scripts/server/calendar'
+import { News } from './scripts/server/news'
+import { Contact } from './scripts/server/contacts'
+
+async function main() {
+    const modules = [
+        'გრაფიკები',
+        'კალენდარი',
+        'სიახლეები',
+        'კონტაქტი',
+    ]
+
+    modules.map((entry, i) => {
+        console.log(`${[i]}.${entry}`)
+    })
+    const answer = await Input('არჩევანი', Number)
+
+    switch (answer) {
+        case 0:
+            await new Charts().Init()
+            break
+        case 1:
+            await new Calendar().Init()
+            break
+        case 2:
+            await new News().Init()
+            break
+        case 3:
+            await new Contact().Init()
+            break
+        default:
+            console.log('არასწორი არჩევანი')
+            break
+    }
+}
+
+main()
